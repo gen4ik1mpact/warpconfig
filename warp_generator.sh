f966c3a64d06a4ac4567b0ea8fa7bb87d2bbbaa5
gen4ik1mpact <cugunder196@gmail.com>
2025-01-08 18:58:20 +0500

    Update warp_generator.sh

--- warp_generator.sh
+++ warp_generator.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 clear
-mkdir -p ~/.cloudshell && touch ~/.cloudshell/no-apt-get-warning # Для Google Cloud Shell, но лучше там не выполнять
+mkdir -p ~/.cloudshell && touch ~/.cloudshell/no-apt-get-warning
 echo "Установка зависимостей..."
-apt update -y && apt install sudo -y # Для Aeza Terminator, там sudo не установлен по умолчанию
-sudo apt-get update -y --fix-missing && sudo apt-get install wireguard-tools jq wget -y --fix-missing # Update второй раз, если sudo установлен и обязателен (в строке выше не сработал)
+apt update -y && apt install sudo -y
+sudo apt-get update -y --fix-missing && sudo apt-get install wireguard-tools jq wget curl -y --fix-missing
 
 priv="${1:-$(wg genkey)}"
 pub="${2:-$(echo "${priv}" | wg pubkey)}"
@@ -12,49 +11,26 @@ api="https://api.cloudflareclient.com/v0i1909051800"
 ins() { curl -s -H 'user-agent:' -H 'content-type: application/json' -X "$1" "${api}/$2" "${@:3}"; }
 sec() { ins "$1" "$2" -H "authorization: Bearer $3" "${@:4}"; }
 response=$(ins POST "reg" -d "{\"install_id\":\"\",\"tos\":\"$(date -u +%FT%T.000Z)\",\"key\":\"${pub}\",\"fcm_token\":\"\",\"type\":\"ios\",\"locale\":\"en_US\"}")
-
 clear
 echo -e "НЕ ИСПОЛЬЗУЙТЕ GOOGLE CLOUD SHELL ДЛЯ ГЕНЕРАЦИИ! Если вы сейчас в Google Cloud Shell, прочитайте актуальный гайд: https://t.me/immalware/1211\n"
-
 id=$(echo "$response" | jq -r '.result.id')
 token=$(echo "$response" | jq -r '.result.token')
 response=$(sec PATCH "reg/${id}" "$token" -d '{"warp_enabled":true}')
 peer_pub=$(echo "$response" | jq -r '.result.config.peers[0].public_key')
-#peer_endpoint=$(echo "$response" | jq -r '.result.config.peers[0].endpoint.host')
 client_ipv4=$(echo "$response" | jq -r '.result.config.interface.addresses.v4')
 client_ipv6=$(echo "$response" | jq -r '.result.config.interface.addresses.v6')
 
-conf=$(cat <<-EOM
-{
-  "interface": {
-   "private_key": "${priv}",
-   "s1": 0,
-   "s2": 0,
-   "j_c": 120,
-   "j_min": 23,
-   "j_max": 911,
-   "h1": 1,
-   "h2": 2,
-   "h3": 3,
-   "h4": 4,
-   "mtu": 1500,
-   "address": "${client_ipv4}, ${client_ipv6}",
-   "dns": "1.1.1.1 , 2606:4700:4700::1111, 1.0.0.1, 2606:4700:4700::1001"
-  },
-  "peer": {
-   "publicKey": "${peer_pub}",
-   "allowed_ips": "0.0.0.0/0, ::/0",
-   "endpoint": "188.114.97.66:3138"
-  }
-}
-EOM
-)
+# Загружаем базовый конфиг из GitHub и заменяем переменные
+config=$(curl -s https://raw.githubusercontent.com/USERNAME/REPO/BRANCH/path/to/file.json)
+conf=$(echo "$config" | sed -e "s/\${priv}/$priv/g" \
+                           -e "s/\${client_ipv4}/$client_ipv4/g" \
+                           -e "s/\${client_ipv6}/$client_ipv6/g" \
+                           -e "s/\${peer_pub}/$peer_pub/g")
 
 echo -e "\n\n\n"
 [ -t 1 ] && echo "########## НАЧАЛО КОНФИГА ##########"
 echo "${conf}"
 [ -t 1 ] && echo "########### КОНЕЦ КОНФИГА ###########"
-
 conf_base64=$(echo -n "${conf}" | base64 -w 0)
 echo "Скачать конфиг файлом: https://immalware.github.io/downloader.html?filename=WARP.conf&content=${conf_base64}"
 echo -e "\n"
